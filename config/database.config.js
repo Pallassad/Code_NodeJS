ccbfb0cf70375540c7ed322bee0f3512aa429cdc
Pallassad <huyct481@gmail.com>
2024-12-17 16:36:38 +0700

    first commit

--- config/database.config.js
+++ config/database.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/users');
+        await mongoose.connect('mongodb+srv://huypham:123123123@cluster0.fci7c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/users');
         console.log('Kết nối MongoDB thành công');
     } catch (err) {
         console.error('Lỗi kết nối MongoDB:', err);
